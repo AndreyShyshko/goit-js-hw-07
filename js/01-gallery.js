a5909e623dbb7b65904c00060aa1ed083e29b58b
Andrey Shyshko <andrey.shyshko84@gmail.com>
2021-12-18 23:31:14 +0200

    написал скрипты lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryImages = document.querySelector(".gallery");
+const galleryMarkup = createGalleryImagesMarkup(galleryItems);
+
+galleryImages.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+galleryImages.addEventListener("click", selectImage);
+
+function selectImage(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") return;
+
+  const selectedImage = event.target.dataset.source;
+
+  const originalImage = basicLightbox.create(`<img src=${selectedImage}>`);
+
+  originalImage.show();
+
+  galleryImages.addEventListener("keydown", modalClose);
+
+  function modalClose(event) {
+    if (event.key !== "Escape") return;
+
+    originalImage.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryImages = document.querySelector(".gallery");
+const galleryMarkup = createGalleryImagesMarkup(galleryItems);
+
+galleryImages.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+     <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+     </a>
+`;
+    })
+    .join("");
+}
+
+const lightboxOptions = {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+const lightboxGallery = new SimpleLightbox(".gallery a", lightboxOptions);
